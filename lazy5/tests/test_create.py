0c45675bf8bc4cc6b3c701eeba25d0d01f915122
Charles H. Camp Jr <ccampjr@gmail.com>
2018-07-06 00:00:26 -0400

    Added test when saving NOT in current folder (pth)

--- lazy5/tests/test_create.py
+++ lazy5/tests/test_create.py
@@ -1,5 +1,6 @@
 """ Test creation of HDF5 files """
 import os
+import time
 
 import pytest
 
@@ -11,7 +12,7 @@ from lazy5.utils import FidOrFile
 
 def test_save_no_attrs():
     data = np.random.randn(20,20)
-    filename = 'temp_save.h5'
+    filename = 'temp_create.h5'
     dset_name = '/Group1/Dset'
     save(filename, dset_name, data, mode='w')
 
@@ -19,7 +20,7 @@ def test_save_no_attrs():
     fid = fof.fid
     assert np.allclose(fid[dset_name], data)
     fof.close_if_file_not_fid()
-    
+
     # Test re-write
     data = np.random.randn(20,20)
     save(filename, dset_name, data, mode='w')
@@ -27,12 +28,12 @@ def test_save_no_attrs():
     fid = fof.fid
     assert np.allclose(fid[dset_name], data)
     fof.close_if_file_not_fid()
-    
+
     # Test re-write when overwrite of dset set to False
     data = np.random.randn(20,20)
     with pytest.raises(IOError):
         save(filename, dset_name, data, dset_overwrite=False)
-        
+
     # Test re-write with attributes
     data = np.random.randn(20,20)
     attr_dict = {'AT1':1, 'AT2':2}
@@ -44,18 +45,31 @@ def test_save_no_attrs():
     assert fid[dset_name].attrs['AT2'] == 2
     with pytest.raises(KeyError):
         fid[dset_name].attrs['DOESNOTEXIST'] == 2
-        
+
     fof.close_if_file_not_fid()
 
-    os.remove(filename)
+    time.sleep(1)
+    try:
+        os.remove(filename)
+    except:
+        print('Could not delete {}'.format(filename))
 
 
+def test_save_diff_path():
+    data = np.random.randn(20,20)
+    filename = 'temp_create2.h5'
+    dset_name = '/Group1/Dset'
+    
+    pth = './temp_test'
+    os.mkdir(pth)
 
+    save(filename, dset_name, data, pth=pth, mode='w')
 
-    # fid['base'].attrs['Attribute_str'] = 'Test'
-    # fid['base'].attrs['Attribute_bytes'] = b'Test'
-    # fid['base'].attrs['Attribute_np_bytes'] = np.bytes_('Test') # pylint: disable=no-member
-    # fid['base'].attrs.create('Attribute_int', 1)
-    # fid['base'].attrs.create('Attribute_float', 1.1)
-    # fid['base'].attrs.create('Attribute_np_1d', np.array([1, 2, 3]))
-    # fid['base'].attrs.create('Attribute_np_2d', np.array([[1, 2, 3], [4, 5, 6]]))
+    fp = os.path.join(pth, filename)
+
+    assert os.path.isdir(pth)
+    assert os.path.isfile(fp)
+    assert os.path.getsize(fp) >= data.nbytes
+
+    os.remove(fp)
+    os.rmdir(pth)
